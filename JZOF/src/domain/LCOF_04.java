258c0fd45a7d55131b66ae8b61090cc7c6bcea72
shen <shenyingge@foxmail.com>
2020-03-01 16:36:38 +0800

    第二次提交

--- JZOF/src/domain/LCOF_04.java
+++ JZOF/src/domain/LCOF_04.java
@@ -1,3 +1,5 @@
+package domain;
+
 public class LCOF_04 {
 
     public static void main(String[] args) {
@@ -7,26 +9,21 @@ public class LCOF_04 {
                           {10, 13, 14, 17, 24},
                           {18, 21, 23, 26, 30}};
         int target = 5;
-        new LCOF_04().findNumberIn2DArray(matrix,target);
+        boolean res = new LCOF_04().findNumberIn2DArray(matrix,target);
+        System.out.println(res);
     }
     public boolean findNumberIn2DArray(int[][] matrix, int target) {
         if(matrix.length == 0) return false;
         if(matrix[0].length == 0)return false;
         int n = matrix.length;
         int m = matrix[0].length;
-        int l = 0;
-        int r = n-1;
-        int mid = 0;
-        while (l < r){
-            mid = l+(r-l)/2;
-            if(matrix[mid][0] < target){
-                l = mid+1;
-            }else if(matrix[mid][0] > target){
-                r = mid-1;
-            }else return true;
-            System.out.printf(mid+" ");
+        int i = 0;
+        int j = m-1;
+        while(i>=0&&i<n&&j>=0&&j<m){
+            if(matrix[i][j] > target)j--;
+            else if(matrix[i][j] < target)i++;
+            else return true;
         }
-        System.out.println(mid);
         return false;
     }
 }
