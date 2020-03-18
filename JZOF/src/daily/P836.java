061c3fe4a97f1ddf723c1fcecbee25285ab35e7f
shen <shenyingge@foxmail.com>
2020-03-18 09:44:15 +0800

    03-18.提交

--- JZOF/src/daily/P836.java
+++ JZOF/src/daily/P836.java
@@ -23,7 +23,6 @@ public class P836 {
         return (rec2[2] <= rec1[0] ||
                 rec2[3] <= rec1[1] ||
                 rec2[0] >= rec1[2] ||
-                rec2[1] >= rec1[3]
-        )? false:true;
+                rec2[1] >= rec1[3] ) ? false : true;
     }
 }
